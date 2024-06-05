26c92d64cd6ab825a704d96f3360991cedd7a0a4
sax <s.apostolico@gmail.com>
2024-06-05 22:46:44 +0200

    updates

--- src/trash/settings.py
+++ src/trash/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from environ import Env
 from pathlib import Path
 
-env = Env()  # type: ignore[no-untyped-call]
+env = Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-imx$*6^8mgosok2jt91@kvdvzfaq3hy-bk%7wb%*8l5m@l^**4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
